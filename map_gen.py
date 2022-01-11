6a2cdbd3f9d3c550bf58eccd5d32b669335d3cf2
Kilian <kilian@senger.group>
2022-01-11 18:34:38 +0100

    1

--- map_gen.py
+++ map_gen.py
@@ -2,12 +2,12 @@ import random
 
 
 def GenMap(add_block, materials):
-    material = random.choice(list(materials.values()))
+    material = random.choice(list(materials.values()))[0]
+
     n = 2 ** 5  # 1/2 width and height of world
     s = 1  # step size
     y = 0  # initial y height
     for x in range(-n, n + 1, s):
-        print(x)
         for z in range(-n, n + 1, s):
             for y1 in range(-n * 2, 0, s):
                 # create a layer stone a grass everywhere.
